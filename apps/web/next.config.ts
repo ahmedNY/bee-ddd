cf5f23d256d825992a6c6c670c66338f03386add
Ahmed Nabil <ahmed.n.yagoub@gmail.com>
2026-03-06 16:08:43 +0300

    initial commit

---
+++ apps/web/next.config.ts
@@ -0,0 +1,9 @@
+import "@bee-ddd/env/web";
+import type { NextConfig } from "next";
+
+const nextConfig: NextConfig = {
+  typedRoutes: true,
+  reactCompiler: true,
+};
+
+export default nextConfig;
---
+++ packages/auth/src/index.ts
@@ -0,0 +1,24 @@
+import { db } from "@bee-ddd/db";
+import * as schema from "@bee-ddd/db/schema/auth";
+import { env } from "@bee-ddd/env/server";
+import { betterAuth } from "better-auth";
+import { drizzleAdapter } from "better-auth/adapters/drizzle";
+
+export const auth = betterAuth({
+  database: drizzleAdapter(db, {
+    provider: "mysql",
+    schema: schema,
+  }),
+  trustedOrigins: [env.CORS_ORIGIN],
+  emailAndPassword: {
+    enabled: true,
+  },
+  advanced: {
+    defaultCookieAttributes: {
+      sameSite: "none",
+      secure: true,
+      httpOnly: true,
+    },
+  },
+  plugins: [],
+});
